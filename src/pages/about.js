5a75a36c87f64401dbb2dc68f15a2c4f36b5354e
mohammad <bargineo@gmail.com>
2023-06-26 23:53:38 -0400

    commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -71,25 +71,25 @@ export default function () {
                       <div className="w-[20vw] bg-m bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-t bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-b bg-cover bg-center h-[60vh]" />
-                      <div className="w-[20vw] bg-r bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-e bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-s bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-m bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-t bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-b bg-cover bg-center h-[60vh]" />
-                      <div className="w-[20vw] bg-r bg-cover bg-center h-[60vh]" />
+                      <div className="w-[20vw] bg-s bg-cover bg-center h-[60vh]" />
+                      <div className="w-[20vw] bg-m bg-cover bg-center h-[60vh]" />
                     </div>
                     <div className="flex justify-around w-[50%]">
+                      <div className="w-[20vw] bg-t bg-cover bg-center h-[60vh]" />
+                      <div className="w-[20vw] bg-b bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-e bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-s bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-m bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-t bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-b bg-cover bg-center h-[60vh]" />
-                      <div className="w-[20vw] bg-r bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-s bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-m bg-cover bg-center h-[60vh]" />
                       <div className="w-[20vw] bg-t bg-cover bg-center h-[60vh]" />
-                      <div className="w-[20vw] bg-b bg-cover bg-center h-[60vh]" />
                     </div>
                   </div>
                 </div>
