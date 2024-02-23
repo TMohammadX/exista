271294e288ccca177165f787108c86b32b9e5902
tmohammadx <bargineo@gmail.com>
2024-02-22 20:57:14 -0800

    cs

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -58,7 +58,7 @@ export default function () {
             animate={{ x: 0 }}
             exit={{ x: "-100vh" }}
             transition={{ type: "spring", stiffness: 100, damping: 20 }}
-            className="absolute top-0 left-0 z-10 grid w-screen h-[100vh] text-white bg-black overflow-hidden"
+            className="absolute top-0 left-0 z-10 grid w-screen h-[100vh] text-white bg-black"
           >
             <div className="grid justify-items-center h-[90vh] gap-[3vh] w-screen">
 
